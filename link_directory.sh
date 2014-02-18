dc8a4da26cdec0a8de430381dc7f9bce90bd61a7
Alexander Curtin <alexander.curtin@temple.edu>
2014-02-18 00:08:28 +0000

    Fixed symlink. I hate git

--- link_directory.sh
+++ link_directory.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ln -s /vagrant/htdocs/RandomFruit/public /var/www/RandomFruit
-chmod -R a+rx /vagrant/htdocs/RandomFruit
+ln -s /vagrant/RandomFruit/public /var/www/RandomFruit
+chmod -R a+rx /vagrant/RandomFruit
 chmod -R a+rx /var/www/RandomFruit
