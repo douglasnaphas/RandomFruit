4a367e89b7167142ce5b642897009dd7da663e9f
cusack <cusack@webDev.localdomain>
2014-03-13 13:48:38 -0400

    Squashed commit of the following:
    commit e19e56e18b87b8fd9d4ce48ee77a27b8d45c22d7
    Author: Greg <gregory.aiello@temple.edu>
    Date:   Wed Mar 12 21:22:54 2014 -0400
    
        Adjusted provisioning script to include phpmyadmin installation and config.

--- provisioning_script.sh
+++ provisioning_script.sh
@@ -3,9 +3,14 @@
 apt-get update
 
 echo 'mysql-server mysql-server/root_password password root' | debconf-set-selections 
-echo 'mysql-server mysql-server/root_password_again password root'| debconf-set-selections 
+echo 'mysql-server mysql-server/root_password_again password root'| debconf-set-selections
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password Durian' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password root' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password Durian' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections 
 
-apt-get -y install php5 mysql-client mysql-server apache2 libapache2-mod-php5  php5-mcrypt git
+apt-get -y install php5 mysql-client mysql-server apache2 libapache2-mod-php5  php5-mcrypt git phpmyadmin
 
 chown -R vagrant:vagrant /var/www
 chmod -R a+X /var/www
@@ -25,8 +30,6 @@ mv composer.phar composer
 chmod a+x composer
 cd $OLDPATH
 
-
-
 chmod -R 664 /var/www
 
 ln -s /vagrant/RandomFruit/public /var/www/RandomFruit
@@ -38,5 +41,8 @@ composer install
 chmod 777 app/storage
 cd -
 
+
+echo '#Include phpmyadmin config' >>/etc/apache2/apache2.conf
+echo 'Include /etc/phpmyadmin/apache.conf' >>/etc/apache2/apache2.conf
 rm -rf /var/lock/apache2
 service apache2 restart
