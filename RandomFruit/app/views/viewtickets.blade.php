ad3ebda945d3b455cde6e8670466499f2d676afc
Alex Curtin <tuc07162@temple.edu>
2014-03-30 16:14:35 -0400

    Created base dash layout and refactored instructordash and viewtickets to be extentions of that layout.

--- RandomFruit/app/views/viewtickets.blade.php
+++ RandomFruit/app/views/viewtickets.blade.php
@@ -1,40 +1,20 @@
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+@extends('dashlayout')
+{{-- app/views/viewtickets.blade.php shows tickets for a given project --}}
 
-    <title>View Tickets - Random Fruit</title>
+@section('title')
+{{ $project->name }} -- View Tickets
+@stop
 
-    <!-- Bootstrap core CSS -->
-    {{HTML::style('includes/css/bootstrap.css')}}
+@section('css')
+	@parent
+	{{HTML::style('includes/css/vtickets.css')}}
+@stop
 
-    <!-- Custom styles for this template -->
-    {{HTML::style('includes/css/vtickets.css')}}
-    {{HTML::style('includes/css/dashboard.css')}}
+@section('page_header')
+View Tickets for {{ $project->name }}
+@stop
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]>
-    <script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
-<!-- Include navigation top bar and side bar -->
-@include('dash/dashnav');
-
-<!-- Begin main content -->
-<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-         <h1 class="page-header">View Tickets</h1>
-
+@section('content')
          
             <div class="vcontain">
                 <form class="form" role="form" method="post">
@@ -113,14 +93,5 @@
                 </tbody>
             </table>    
         </div>
-     </div>
-</div>
-<!-- End main content -->
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="includes/js/bootstrap.min.js"></script>
-<script src="../../assets/js/docs.min.js"></script>
-</body>
-</html>
+	 </div>
+@stop
