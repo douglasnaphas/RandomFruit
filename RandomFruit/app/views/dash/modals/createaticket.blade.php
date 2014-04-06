7da7e23f76b3c308a8256fc6ba061089eda15174
Alex Curtin <tuc07162@temple.edu>
2014-04-06 17:58:54 -0400

    Added owner selecor to createaticket.

--- RandomFruit/app/views/dash/modals/createaticket.blade.php
+++ RandomFruit/app/views/dash/modals/createaticket.blade.php
@@ -6,13 +6,61 @@
                 <h4 class="modal-title" id="myModalLabel">Create a Ticket</h4>
             </div>
            <form class="form" role="form" data-async data-target="createTicket" data-modal-id="createTicket" id="ct_form" action="{{URL::action('TicketController@createticketAction')}}" method="post">
-                <div class="modal-body">
+		   <div class="modal-body">
+			   <script>
+				   function projectChange( project_id ){
+					   var owner_select = document.getElementById('owner');
+					   <?php
+					   $available_options = array();
+					   foreach(Auth::user()->projects as $project){
+					   foreach($project->users as $user){
+					   $available_options[] = array('name' => $user->username,
+					   'id' => $user->id, 'project_id' => $project->id
+					   );
+
+
+					   }
+					   }
+					   
+					   echo "var available_options = " . json_encode($available_options, true) . ";\n";
+
+					   ?>
+					   for(var i = 0; i < owner_select.options.length; i++){
+						   var current_option = owner_select.options[i];
+						   if(current_option.getAttribute('data-project') == project_id){
+							   continue;
+						   }
+						   else{
+							   current_option.remove();
+						   }
+					   }
+					   for(var i = 0; i < available_options.length; i++){
+						   var current_option = available_options[i];
+						   if(current_option.project_id == project_id){
+							   owner_select.innerHTML += '<option value="' + current_option.user + '" data-project="' +
+							   current_option.project_id + '">' + current_option.name + "</option>";
+						   }
+					   }
+
+				   }
+
+			   </script>
 		    <div class="form-group" id="project-input">
 			<label for="project-input">Project</label>
-			<select id="project" name="project">
+			<select id="project" name="project" onchange="projectChange(document.getElementById('project').options[document.getElementById('project').selectedIndex].value)">
 				@foreach(Auth::user()->projects as $project)
-       					<option value="{{ $project->id}}">{{$project->name}}</option>
+       					<option value="{{ $project->id }}">{{ $project->name }}</option>
 				@endforeach
+			</select>
+		</div>
+		    <div class="form-group" id="owner-input">
+			<label for="owner-input">Project</label>
+			<select id="owner" name="owner">
+				<?php $first_project = Auth::user()->projects->first() ?>
+				@foreach($first_project->users as $user)
+				<option value="{{$user->id}}" data-project="{{$first_project->id}}">{{$user->username}}</option>
+				@endforeach
+				
 			</select>
 		    </div>
                     <div class="form-group" id="title-input">
