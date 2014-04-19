7b5aee4bf78d8ba6bea0ca2e7cff11c61e01192b
Alex Curtin <ViperX120@gmail.com>
2014-04-19 12:41:16 -0400

    Added first workLogController test

--- RandomFruit/app/controllers/WorkLogController.php
+++ RandomFruit/app/controllers/WorkLogController.php
@@ -5,8 +5,10 @@ class WorkLogController extends BaseController
 	public function addWorkLog($project_name, $ticket_number){
 		$project;
 		$ticket;
+		$successArray = array("status" => "success");
+		$failureArray = array("status" => "fail");
 		if(!($project = Project::fromName($project_name)){
-			return new Resoponse("The selected project does not exist", 404); 
+			return new Response("The selected project does not exist", 404); 
 		}
 		else if(!($ticket = $project->getTicketFromNumber($ticket_number)){
 			return new Response("The selected ticket does not exist", 404); 
@@ -17,8 +19,18 @@ class WorkLogController extends BaseController
 			$work_log->week_id = Input::get('week');
 			$work_log->value = Input::get('hours_worked');
 			$ticket->workLogs()->save($work_log);
+			$payload = array("status" => "success", 
+				"data" => array(
+					"work_log" => $work_log->toArray(),
+					"actual_hours" =>  $ticket->computeActualHours()
+				)
+			);
+
+			return new Response::JSON($payload, 200);
 		}catch(Exception $e){
-			return new Response("A server side error occurred", 504);
+			$failureArray['message'] = "A server side error occurred";
+			$failureArray['debug'] = $e->getMessage();
+			return new Response::JSON($failureArray, 504);
 		}
 
 
---
+++ RandomFruit/app/tests/WorkLogControllerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+class WorkLogController extends TestCase{
+	public function setUp(){
+		parent::setUp();
+		Artisan::call('migrate:reset');
+		Artisan::call('migrate');
+		$this->seed();
+	}
+
+	public function testUpdateWorkLogOnce(){
+		//Log in as jeff
+		$jeff = User::fromName('jeff');
+		$project = Project::fromName('RandomFruit');
+		$this->be($jeff);
+		//Create a ticket (not from the frontend)
+		$ticket = new Ticket();
+		$ticket->title = "Work log test";
+		$title->description = "";
+		$title->owner_id = $jeff->id;
+		$ticket->creator_id = $jeff->id;
+		$ticket->planned_hours = 4.0;
+		$ticket->project = $project->id;
+		$ticket->save();
+
+		//update ticket to get ticket number
+		$ticket = Ticket::find($ticket->id);
+
+		//Post to the ticket controller a work log entry for 4 hours
+		$post_data = array(
+			"week" => $project->weeks()->get()->first(),
+			"actual_hours" => "4.0"
+		);
+
+		$response = $this->route(
+			"addWorkLog", //route alias
+			array("project_name", => $project->name, "ticket_number" => $ticket->number), //url parameters,
+			$post_data //well, post data
+		);
+			
+		//Ensure that the response succeeds
+		$this->assertResponseOk();
+
+		//Assert that the actual hours == 4.0 hours
+		$response_json = json_decode($response->getContent());
+		AssertEquals($response_json->data->actual_hours == 4.0);
+
+
+		//Assert that the work log has the same user_id as jeff
+		AssertEquals($response_json->work_log->user_id == $jeff->id);
+	}
+
+	/*
+	public function testUpdateWorkLogMultipleTimes(){
+		//Log in as admin
+		//Create a ticket (not from the frontend)
+		//Post to the ticket controller a work log entry for 4 hours
+		//Ensure that the response succeeds
+		//Assert that the actual hours == 4.0 hours
+		//Post to the ticket controller a work log entry for 4 hours
+		//Ensure that the response succeeds
+		//Assert that the actual hours == 8.0 hours
+		//Post to the ticket controller a work log entry for 4 hours
+		//Ensure that the response succeeds
+		//Assert that the actual hours == 12.0 hours
+	}
+	 */
+
+}
