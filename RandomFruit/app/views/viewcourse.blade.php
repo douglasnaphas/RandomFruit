afe61b699bfb676fcb3728f391c84a4c66cf907e
JRams <jtr31993@gmail.com>
2014-04-20 14:13:10 -0400

    Course Shell

--- RandomFruit/app/views/viewcourse.blade.php
+++ RandomFruit/app/views/viewcourse.blade.php
@@ -15,23 +15,44 @@ View Courses
 
 @section('content')
 
-<div class="vcontain">
+<div class="ccontain">
+    @foreach(Course::all() as $course)
+    <h3>  {{  $course->code  }}  </h3>
     <div class="table-responsive">
         <table class="table table-striped table-hover">
             <thead>
             <tr>
-                
+                <th>Projects</th>
             </tr>
             </thead>
             <tbody>
+                @foreach($course->projects as $project)
                 <tr>
                     <td>
-                        
+                        {{ $project->name }} 
+                    </td>
+                    <td>
+                    </td>
+                    <td>
+                    </td>
+                    <td>
                     </td>
                 </tr>
+                <tr> 
+                    <td>
+                        <strong> Team Members: </strong>
+                    </td>
+                    <td>Jeff</td>
+                    <td>Doug</td>
+                    <td>Alex</td>
+                    <td>Greg</td>
+                    <td>Wolfgang</td>
+                </tr>
+                @endforeach
             </tbody>
         </table>    
     </div>
+    @endforeach
 </div>
 
 @stop
