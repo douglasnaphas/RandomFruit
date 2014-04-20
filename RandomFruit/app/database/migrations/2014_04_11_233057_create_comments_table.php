d66d7b04aeb68377c0c6265a53792adad9c92f6f
Alex Curtin <ViperX120@gmail.com>
2014-04-20 19:07:17 -0400

    Comments are now deleted when thier tickets are deleted

--- RandomFruit/app/database/migrations/2014_04_11_233057_create_comments_table.php
+++ RandomFruit/app/database/migrations/2014_04_11_233057_create_comments_table.php
@@ -13,13 +13,14 @@ class CreateCommentsTable extends Migration {
 	public function up()
 	{
 		Schema::create('comments', function(Blueprint $table)
-		{
+        {
+            $table->engine = 'InnoDB';
 			$table->increments('id');
 			$table->timestamps();
-			$table->integer('user_id')->unsigned();
-			$table->foreign('user_id')->references('id')->on('users');
+			$table->integer('user_id')->unsigned()->nullable();
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('set null')->nullable();
 			$table->integer('ticket_id')->unsigned();
-			$table->foreign('ticket_id')->references('id')->on('tickets');
+			$table->foreign('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
 			$table->mediumText('content');
 		});
 	}
