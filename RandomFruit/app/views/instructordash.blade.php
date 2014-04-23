a4f3798a0cec671182f7073252a0380e8950ce9b
JRams <jtr31993@gmail.com>
2014-04-23 18:43:31 -0400

    Repeating graphs but broken

--- RandomFruit/app/views/instructordash.blade.php
+++ RandomFruit/app/views/instructordash.blade.php
@@ -12,54 +12,52 @@ Burndown Chart
 @section('content')
 <?php
 $curDate = date("mdy_His");
-?>
-<div>
-    <canvas id="canvas" height="500" width="1000"></canvas>
-    <script>
-        var lineChartData = {
-			labels: {{json_encode(Project::fromName('RandomFruit')->weeksLegendArray())}},
+    echo "<div> \n";
+    foreach(Auth::user()->projects as $project){
+        echo " <canvas id='".$project->id."canvas' height='500' width='1000'></canvas> \n";
+        echo "<script> \n";
+        echo "var lineChartData = {
+			labels:".json_encode(Project::fromName("$project->name")->weeksLegendArray()).",
             datasets: [
                 {
-                    fillColor: "rgba(0,0,0,0)",
-                    strokeColor: "rgba(5,162,5,1)",
-                    pointColor: "rgba(5,162,5,1)",
-                    pointStrokeColor: "#fff",
-                    data: {{json_encode(Project::fromName('RandomFruit')->getPlannedValueData())}},
-                    title: "Planned"
+                    fillColor: 'rgba(0,0,0,0)',
+                    strokeColor: 'rgba(5,162,5,1)',
+                    pointColor: 'rgba(5,162,5,1)',
+                    pointStrokeColor: '#fff',
+                    data: ".json_encode(Project::fromName("$project->name")->getPlannedValueData()).",
+                    title: 'Planned'
                 },
                 {
-                    fillColor: "rgba(0,0,0,0)",
-                    strokeColor: "rgba(77,50,205,1)",
-                    pointColor: "rgba(77,50,205,1)",
-                    pointStrokeColor: "#fff",
-                    data: {{json_encode(Project::fromName('RandomFruit')->getActualValueData())}},
-                    title: "Actual"
+                    fillColor: 'rgba(0,0,0,0)',
+                    strokeColor: 'rgba(77,50,205,1)',
+                    pointColor: 'rgba(77,50,205,1)',
+                    pointStrokeColor: '#fff',
+                    data: ".json_encode(Project::fromName("$project->name")->getActualValueData()).",
+                    title: 'Actual'
                 },
                 {
-                    fillColor: "rgba(0,0,0,0)",
-                    strokeColor: "rgba(220,5,5,1)",
-                    pointColor: "rgba(220,5,5,1)",
-                    pointStrokeColor: "#fff",
-                    data: {{json_encode(Project::fromName('RandomFruit')->getEarnedValueData())}},
-                    title: "Earned"
+                    fillColor: 'rgba(0,0,0,0)',
+                    strokeColor: 'rgba(220,5,5,1)',
+                    pointColor: 'rgba(220,5,5,1)',
+                    pointStrokeColor: '#fff',
+                    data: ".json_encode(Project::fromName("$project->name")->getEarnedValueData()).",
+                    title: 'Earned'
                 }
             ]
-        };
-
-        var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData, {showLegend: true});
-        //This should produce a button that allows for the graph to be saved
-        var graph = document.getElementById("canvas");
-        var cs = new CanvasSaver('./saveme.php');
-        var btn = cs.generateButton("Save Graph", graph, "BurndownChart_<?php echo $curDate; ?>");
-
-    </script>
-    <div id="saveChartBtn" class="button-container"></div>
-
-    <script>
-        var saveBtnDiv = document.getElementById('saveChartBtn');
-        saveBtnDiv.appendChild(btn);
-    </script>
-</div>
+        };\n";
+        echo "var myLine = new Chart(document.getElementById('".$project->id."canvas').getContext('2d')).Line(lineChartData, {showLegend: true});\n";
+        echo "var graph = document.getElementById('".$project->id."canvas');\n";
+        echo "var cs = new CanvasSaver('./saveme.php');\n";
+        echo "var btn = cs.generateButton('Save Graph', graph, 'BurndownChart_$curDate');\n";
+        echo "</script>\n";
+        echo"<div id=\"saveChartBtn\" class=\"button-container\"></div>\n";
+        echo"<script>
+            var saveBtnDiv = document.getElementById('saveChartBtn');
+            saveBtnDiv.appendChild(btn);
+            </script>\n";
+        echo"</div>\n";
+}
+?>  
 
 <h2 class="sub-header">Owned Tickets</h2>
 
