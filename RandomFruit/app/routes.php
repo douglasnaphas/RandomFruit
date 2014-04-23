084fd8b7a86467a29bc33eae5da4358fcfda2bfb
Alex Curtin <tuc07162@temple.edu>
2014-04-23 16:38:23 -0400

    User creation implemented on backend

--- RandomFruit/app/routes.php
+++ RandomFruit/app/routes.php
@@ -52,6 +52,9 @@ Route::post('api/add_work_log/{project_name}/{ticket_number}', array('as' => 'ad
 //Modifying courses
 Route::post('api/create_course', array('as' => 'createCourse', 'uses' => 'CourseController@createCourse'));
 
+//Creating users
+Route::post('api/create_user', array('as' => 'createUser', 'uses' => 'UserController@createUser'));
+
 Route::any('dash', array('as' => 'dash', 'before' => 'user_only', function(){
 	return View::make('instructordash');
 }));
