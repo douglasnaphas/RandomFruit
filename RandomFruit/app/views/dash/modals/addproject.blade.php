bf59c78712704b1132490e1b0d78b34a9458aa8a
Alex Curtin <tuc07162@temple.edu>
2014-04-23 19:27:21 -0400

    Projects can now be created from the front end.

--- RandomFruit/app/views/dash/modals/addproject.blade.php
+++ RandomFruit/app/views/dash/modals/addproject.blade.php
@@ -5,16 +5,22 @@
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title" id="myModalLabel">Add a Project to a Course</h4>
             </div>
-            <form class="form" role="form" data-async data-target="addProject" data-modal-id="addProject" method="post">
+			<form class="form" role="form" data-target="addProject" data-modal-id="addProject" method="post"
+				id="createProject-form" action="{{URL::route('createProject')}}">
                 <div class="modal-body">
                     <div class="form-group" id="project-input">
                         <label for="project-name">Project Name</label>
                         <input type="text" class="form-control" placeholder="Enter project name" id="project-name"
-                               name="project-name" required>
+                               name="project_name" required>
+                    </div>
+                    <div class="form-group" id="project-description">
+                        <label for="project-description">Project Description</label>
+						<input type="textarea" class="form-control" placeholder="Enter project name" 
+						id="project-description" name="project_description" required>
                     </div>
                      <div class="form-group">
                         <label for="course">Course</label>
-                        <select id="course" name="course" class="form-control">
+                        <select id="course" name="course_id" class="form-control">
                             @foreach(Course::all() as $course)
                             <option value="{{$course->id}}">
                             {{"$course->code"}}
@@ -28,6 +34,32 @@
                 </div>
                 </div>
             </form>
+			<script type="text/javascript">
+                $(function(){
+                            var $createProject = $('#createProject-form');
+                            $createProject.on('submit', function(event){
+                                var $form = $(this);
+                                $.ajax({
+                                    type: "POST",
+                                    data: $form.serialize(),
+                                    type: $form.attr('method'),
+                                    url: $form.attr('action'),
+
+                                    success:  function (data, status) {
+                                        $('#actual-value').html(data.data.actual_hours);
+                                        $('#'+$form.attr('data-modal-id')).modal('hide');
+                                    },
+
+                                    error:  function (request, status, error) {
+                                        alert(request.responseText);
+                                        alert("Please check your submission and try again.");
+                                    }
+
+                                });
+                                event.preventDefault();
+                            });
+                });
+                </script>
         </div>
     </div>
 </div>
