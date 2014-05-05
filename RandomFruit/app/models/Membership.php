2f829861fc849046dadebac8d1dda0f6b06a10ea
Alex Curtin <tuc07162@temple.edu>
2014-05-04 20:57:32 -0400

    Added phpdoc comments to Membership model

--- RandomFruit/app/models/Membership.php
+++ RandomFruit/app/models/Membership.php
@@ -1,6 +1,8 @@
 <?php
 
-
+/**
+ * The data access object for the memberships table
+ */
 class Membership extends Eloquent {
 
 	/**
@@ -8,11 +10,22 @@ class Membership extends Eloquent {
 	 *
 	 * @var string
 	 */
-	protected $table = 'memberships';
+    protected $table = 'memberships';
+
+    /**
+     * Gets the relationship to the project
+     *
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
 	public function project(){
 		return $this->hasOne('Project');
 	}
 
+    /**
+     * Gets the relationship to the User
+     *
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
 	public function user(){
 		return $this->hasOne('User');
 	}
